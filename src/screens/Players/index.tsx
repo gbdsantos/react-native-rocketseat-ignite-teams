e69590ea4f7fc0dc7a8d3ec4130a6e14e86ec4bf
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-10-07 23:51:10 -0300

    🎨 styles(mobile): create ButtonIcon component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from './styles';
 
+import { ButtonIcon } from '@components/ButtonIcon';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 
@@ -11,6 +12,8 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <ButtonIcon />
     </Container>
   );
 }

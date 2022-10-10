5467075782d32926cecb1e0201512bfd387676c0
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-10-10 08:38:12 -0300

    ✍️  update(mobile): finish players list

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -8,16 +8,18 @@ import {
   MumbersOfPlayers
 } from './styles';
 
+import { Button } from '@components/Button';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Guilherme', 'Maria']);
+  const [players, setPlayers] = useState([]);
 
   return (
     <Container>
@@ -56,6 +58,10 @@ export function Players() {
       </HeaderList>
 
       <FlatList
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
         data={players}
         keyExtractor={item => item}
         renderItem={({ item }) => (
@@ -64,6 +70,17 @@ export function Players() {
             onRemove={() => { }}
           />
         )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Não há pessoas nesse time."
+          />
+        )}
+      />
+
+      <Button
+        title="Remover Turma"
+        type="SECONDARY"
       />
     </Container>
   );

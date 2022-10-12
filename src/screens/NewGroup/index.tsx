7a0b25070e6918e0d042cf44c34d306d608e09b9
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-10-12 13:50:34 -0300

    ⭐ feat(mobile): handle exceptions

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { AppError } from '@utils/AppError';
 
 import { groupCreate } from '@storage/group/groupCreate';
 
@@ -20,11 +22,20 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+      }
+
       await groupCreate(group);
       navigate('players', { group });
 
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message);
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+        console.log(error);
+      }
     }
   }
 

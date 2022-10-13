69ab53dcb2dd5f9143a51575e8eed614070c8c1b
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-10-13 14:09:38 -0300

    ✨ feat(mobile): list players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,5 +1,5 @@
+import { Alert, FlatList } from 'react-native';
 import { useState } from 'react';
-import { FlatList } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import {
@@ -18,16 +18,46 @@ import { Input } from '@components/Input';
 import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 
+import { AppError } from '@utils/AppError';
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { playersGetByGroup } from '@storage/player/playersGetByGroup';
+
 type RouteParams = {
   group: string;
 }
 
 export function Players() {
+  const [newPlayerName, setNewPlayerName] = useState('');
   const [team, setTeam] = useState('Time A');
   const [players, setPlayers] = useState([]);
 
   const route = useRoute();
   const { group } = route.params as RouteParams;
+
+  async function handleAddPlayer() {
+    if (newPlayerName.trim().length === 0) {
+      return Alert.alert('Nova pessoa', 'Informe o nome da pessoa para adicionar.')
+    }
+
+    const newPlayer = {
+      name: newPlayerName,
+      team,
+    }
+
+    try {
+      await playerAddByGroup(newPlayer, group);
+      const players = await playersGetByGroup(group);
+      console.log(players);
+
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Nova pessoa', error.message)
+      } else {
+        console.log(error);
+        Alert.alert('Nova pessoa', 'Não foi possível adicioanr');
+      }
+    }
+  }
 
   return (
     <Container>
@@ -41,9 +71,11 @@ export function Players() {
         <Input
           autoCorrect={false}
           placeholder="Nome da pessoa"
+          onChangeText={setNewPlayerName}
         />
         <ButtonIcon
           icon="add"
+          onPress={handleAddPlayer}
         />
       </Form>
 
